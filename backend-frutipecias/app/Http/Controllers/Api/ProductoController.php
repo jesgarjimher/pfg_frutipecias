373bd0c750b7b6f98e8893eca7cb5b6b30a60114
Jesus Garcia <jesgarjimher@gmail.com>
2026-03-29 23:05:35 +0200

    ft:created models and listAll controller

---
+++ backend-frutipecias/app/Http/Controllers/Api/ProductoController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Producto;
+use Illuminate\Http\Request;
+
+class ProductoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function listAll()
+    {
+        $productos = Producto::with(["categoria", "informacionNutricional"])->get();
+        return response()->json($productos);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ backend-frutipecias/app/Models/InformacionNutricional.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class InformacionNutricional extends Model
+{
+    //definicion del nombre de la tabla
+    protected $table = "informacion_nutricional";
+}
