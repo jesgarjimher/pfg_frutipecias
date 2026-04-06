495c518b1204ca18db6f4a4687c68d5fb13096e1
Jesus Garcia <jesgarjimher@gmail.com>
2026-04-06 12:12:17 +0200

    ft:fixed nested <a> links, added logo

--- frontend-frutipecias/src/components/common/NavBar.js
+++ frontend-frutipecias/src/components/common/NavBar.js
@@ -2,27 +2,25 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
+          <img src="/logo-frutipecias.png"  width="120" height="120" className="d-inline-block align-top me-2" alt="Logo Frutipecias" /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about-us">Sobre nosotros</Nav.Link>
+            <NavDropdown title="Productos" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/productos/frutos-secos">Frutos secos</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/productos/frutas-deshidratadas">Frutas deshidratadas</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/productos/especias">Especias</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+              <NavDropdown.Item  as={Link} to="/productos">Todos</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
