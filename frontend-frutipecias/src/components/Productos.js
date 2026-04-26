ebf6f4ec4bf04a4642767c6b2b3c4cb45345d136
Jesus Garcia <jesgarjimher@gmail.com>
2026-04-26 21:46:56 +0200

    ft: added placeholder cards to Product component

--- frontend-frutipecias/src/components/Productos.js
+++ frontend-frutipecias/src/components/Productos.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Placeholder } from "react-bootstrap";
 
 
 function Productos() {
@@ -12,8 +12,10 @@ function Productos() {
     const [pagina, setPagina] = useState(1);
     const [paginacion, setPaginacion] = useState({});
     const [busqueda, setBusqueda] = useState("");
+    const [cargando, setCargando] = useState(true);
     
     const getProductos = async (pagina = 1) => {
+        setCargando(true);
         try {
             let url = `http://127.0.0.1:8000/api/productos?page=${pagina}`;
 
@@ -36,6 +38,8 @@ function Productos() {
             console.error("Error detallado:", error);
             const msg = error.response?.data?.message || "Error de conexión o CORS";
             alert(msg);
+        }finally {
+            setCargando(false);
         }
     }
 
@@ -61,17 +65,42 @@ function Productos() {
                 <input type="text" placeholder="buscar..." value={busqueda} onChange={(e) => setBusqueda(e.target.value)}></input>
             </div>
             <h1>{categoria}</h1>
-            {productos.map((producto, index) => (
-                <Card style={{ width: '18rem' }} key={producto.id}>
-                    <Card.Img variant="top" src={pathStorageImg + producto.imagen} />
-                    <Card.Body>
-                        <Card.Title>{producto.nombre}</Card.Title>
-                        <Card.Text>
-                            {producto.categoria.nombre}
-                        </Card.Text>
-                        <Button variant="primary">Ver más</Button>
-                    </Card.Body>
-                </Card>
+            <div className="row">
+                {cargando ? (
+                    [1,2,3,4,5,6,7,8].map((n) => (
+                        <div className="col-12 col-md-6 col-xl-3 mb-4" key={n}>
+                            <Card style={{ width: '18rem' }}>
+                                {/* Un div gris que simula la imagen */}
+                                <div className="placeholder-glow">
+                                    <div className="placeholder col-12" style={{ height: '180px' }}></div>
+                                </div>
+                                <Card.Body>
+                                    <Placeholder as={Card.Title} animation="glow">
+                                        <Placeholder xs={6} />
+                                    </Placeholder>
+                                    <Placeholder as={Card.Text} animation="glow">
+                                        <Placeholder xs={7} /> <Placeholder xs={4} />
+                                        <Placeholder xs={4} />
+                                    </Placeholder>
+                                    <Placeholder.Button variant="primary" xs={6} />
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    ))
+                ) : (
+                    productos.map((producto, index) => (
+                <div className="col-12 col-md-6 col-xl-3" key={producto.id}>
+                    <Card style={{ width: '18rem' }} key={producto.id}>
+                        <Card.Img variant="top" src={pathStorageImg + producto.imagen} />
+                        <Card.Body>
+                            <Card.Title>{producto.nombre}</Card.Title>
+                            <Card.Text>
+                                {producto.categoria.nombre}
+                            </Card.Text>
+                            <Button variant="primary">Ver más</Button>
+                        </Card.Body>
+                    </Card> 
+                </div>
                 // <div key={producto.id}>
                 //     <p>{index+1}-{producto.nombre}</p>
                 //     <p>{producto.categoria.nombre}</p>
@@ -80,7 +109,11 @@ function Productos() {
                 // </div>
                 
 
-            ))}
+            ))
+                )};
+            
+            </div>
+
             <div>
                 {botonesPaginacion}
             </div>
